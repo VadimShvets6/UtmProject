f876f66607269ce6ceb7ff9f6b958c0c02015a86
Vadim Shvets <vadimshvets@gmail.com>
2023-03-30 00:09:49 +0300

    added icon cart + click + state

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
+import {FaShoppingCart} from 'react-icons/fa'
 
 export default function Header() {
+    let [cartOpen, setCartOpen] = useState(false)
     return (
         <header>
             <div>
@@ -10,6 +12,8 @@ export default function Header() {
                     <li>Контакты</li>
                     <li>Кабинет</li>
                 </ul>
+                <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)}
+                                className={`shop-cart-button ${cartOpen && 'active'}`}/>
             </div>
             <div className='presentation'></div>
         </header>
